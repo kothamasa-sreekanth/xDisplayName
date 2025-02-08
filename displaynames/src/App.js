a32978d0c6c37e3dffa2dd19051574b1e4293108
kothamasa_sreekanth <ksreekanth703@gmail.com>
2025-02-08 08:29:55 +0530

    first commit

---
+++ displaynames/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import DisplayNames from './components/DisplayNames';
+function App() {
+  return (
+  <>
+  <DisplayNames />
+  </>
+  );
+}
+
+export default App;
---
+++ displaynames/src/components/DisplayNames.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+const DisplayNames = () => {
+  const [FirstName, setFirstName] = useState("");
+  const [LastName, setLastName] = useState("");
+  const [FullName, setFullName] = useState("");
+
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    if (FirstName.trim() && LastName.trim()) {
+      setFullName(`${FirstName} ${LastName}`);
+      setFirstName("");
+      setLastName("");
+
+    }
+  };
+
+  const handleClearFullName = () => {
+    setFullName(""); // ✅ Clear full name when first name input is clicked
+  };
+
+  return (
+    <div>
+      <center>
+        <h1>Full Name Display</h1>
+        <form onSubmit={handlesubmit}>
+          <div>
+            <label>First Name : </label>
+            <input className='Firstname' type="text" placeholder='First Name' value={FirstName} onChange={(e)=>setFirstName(e.target.value)} onClick={handleClearFullName} />
+          </div>
+
+          <div>
+            <label>Last Name : </label>
+            <input className='Lastname' type="text" placeholder='Last Name' value={LastName} onChange={(e)=>setLastName(e.target.value)} />
+          </div>
+          <button type='submit'>Submit</button>
+        </form>
+        
+       {FullName && <h1>Full Name: {FullName}</h1>}
+      </center>
+    </div>
+  )
+}
+
+
+export default DisplayNames;
